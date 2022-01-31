aa857ff47f323e94d5ccc770692730f15610e39e
d-karpov <77918194+d-karpov@users.noreply.github.com>
2022-01-31 20:24:47 +0300

    Initial Commit

---
+++ SliderQuizApp/SliderQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderQuizAppApp.swift
+//  SliderQuizApp
+//
+//  Created by Денис Карпов on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
